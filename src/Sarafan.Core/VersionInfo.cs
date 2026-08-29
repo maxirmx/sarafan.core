7d10895380feab1959cb4dcc0776854d12f78fff
Maxim Samsonov <m.samsonov@computer.org>
2026-08-30 00:07:24 +0300

    wip

--- src/Sarafan.Core/VersionInfo.cs
+++ src/Sarafan.Core/VersionInfo.cs
@@ -2,9 +2,16 @@
 // All rights reserved.
 // This file is a part of the Sarafan application
 
+using System.Reflection;
+
 namespace Sarafan.Core;
 
 public static class VersionInfo
 {
-    public const string AppVersion = "0.0.2";
+    public static string AppVersion { get; } =
+        typeof(VersionInfo).Assembly
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+            .InformationalVersion
+        ?? typeof(VersionInfo).Assembly.GetName().Version?.ToString(3)
+        ?? "Unknown";
 }
